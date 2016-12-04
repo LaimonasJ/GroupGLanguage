8f4c0a5dbf1a872ee0a9d86892aa630a087d0c41
Thompsonas <vytautasveg@yahoo.com>
2016-12-04 19:22:27 +0200

    Semantics update
    Semantics now detects the first declaration line and attempts to
    visualize push command calls

--- Semantics/src/Semantics.java
+++ Semantics/src/Semantics.java
@@ -8,6 +8,8 @@ public class Semantics
    ArrayList<String> parserTree = new ArrayList();
    ArrayList<Type> typeList = new ArrayList();
    ArrayList<Block> functionList = new ArrayList();
+   ArrayList<Variable> tempList = new ArrayList();
+   static int tempCount = 1;
    
    public Semantics(String parserTree)
    {
@@ -17,9 +19,12 @@ public class Semantics
          if(!s.trim().isEmpty())
          {
             this.parserTree.add(s.trim());
-            System.out.println(s.trim());
+            //System.out.println(s.trim());
+            
          }
       }
+      
+
       
       typeList.add(new Type("string"));
       typeList.add(new Type("int"));
@@ -28,6 +33,48 @@ public class Semantics
    
    public void run()
    {
+      Block mainBlock = new Block(null, "main", null);
+      int index=(parserTree.indexOf("<main>"));
+      parseRemoveRange(0, index, parserTree);
+      //System.out.println(parserTree.toString());
+      index=(parserTree.indexOf("<statement>"));
+      parseRemoveRange(0, index, parserTree);
+      //System.out.println(parserTree.toString());
+      index=1;
+      // System.out.println(parserTree.get(index));
       
+      if (parserTree.get(index+1).equals("<declarator>"))
+      { 
+          String varType = parserTree.get(index+2);
+          System.out.println(varType);
+          int identifierIndex = parserTree.indexOf("<identifier-list>")+1;
+          mainBlock.variableList.add(new Variable(parserTree.get(identifierIndex), typeList.get(1)));
+          
+          System.out.println("PUSH " + parserTree.get(identifierIndex));
+          identifierIndex++;
+          while (!parserTree.get(identifierIndex).equals("</identifier-list>"))
+          {
+              identifierIndex+=2;
+              //System.out.println(parserTree.get(identifierIndex));
+              //if (!mainBlock.variableList.contains(parserTree.get(indentifierIndex))
+              System.out.println("PUSH " + parserTree.get(identifierIndex));
+              identifierIndex++;
+          }
+      }
+      
+      
+       
    }
+   
+   public void parseRemoveRange (int fromIndex, int toIndex, ArrayList<String> parserTree)
+   {
+             while (fromIndex <= toIndex)
+   {
+       parserTree.remove(fromIndex);
+       fromIndex++;
+   } 
+             
+   }
+   
+   
 }
--- Semantics/src/Variable.java
+++ Semantics/src/Variable.java
@@ -4,4 +4,10 @@ class Variable
 {
    public String name;
    public Type type;
+   
+    Variable(String name, Type type)
+   {
+      this.name = name;
+      this.type = type;
+   }
 }
